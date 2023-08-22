c3b807dbaacccb34379177fc031bca42c34b00bf
adamocallaghan <adam.ocallaghan@yahoo.ie>
2023-08-22 15:55:40 +0100

    first commit

---
+++ test/BasicNFTTest.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
+
+contract BasicNFTTest is Test {}
