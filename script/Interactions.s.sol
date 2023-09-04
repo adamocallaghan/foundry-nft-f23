159d1053ed3331feff5790f1061ca12ad820b63d
adamocallaghan <adam.ocallaghan@yahoo.ie>
2023-09-04 14:06:46 +0100

    Interactions complete

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,9 +3,23 @@ pragma solidity ^0.8.18;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNft is Script {
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
     function run() external {
-        // INSTALL FOUNDRY DEVOPS IS THE NEXT STEP :-)
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNFT(contractAddress).mintNft(PUG);
+        vm.stopBroadcast();
     }
 }
